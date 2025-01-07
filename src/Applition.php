51e223c1763ceaf6026492107fa3c8e1e6a44927
Liang INX <liang@in-x.cc>
2025-01-07 20:19:09 +0800

    空参数抛出异常

--- src/Applition.php
+++ src/Applition.php
@@ -2,6 +2,8 @@
 
 namespace BaiduMapSdk;
 
+use BaiduMapSdk\Exception\BaiduMapSdkException;
+
 abstract class Applition
 {
 
@@ -10,6 +12,12 @@ abstract class Applition
 
     public function __construct(string $ak, int $default_service_id)
     {
+        if (empty($ak)) {
+            throw new BaiduMapSdkException('ak 不能为空');
+        }
+        if (empty($default_service_id)) {
+            throw new BaiduMapSdkException('default_service_id 不能为空');
+        }
         $this->ak = $ak;
         $this->defaultServiceId = $default_service_id;
     }
